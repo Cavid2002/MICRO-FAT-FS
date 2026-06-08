a805a9653a5a525a5f253c915e3be620c374a1a9
Anar Bastanov <anar.bastanov.out@gmail.com>
2026-06-08 16:09:48 +0400

    fopen done

--- common.h
+++ common.h
@@ -13,7 +13,7 @@ uint32_t memset(uint8_t* ptr, uint8_t c, uint32_t size);
 uint32_t memcpy(uint8_t* src, uint8_t* dest, uint32_t count);
 int memcmp(uint8_t* ptr1, uint8_t* ptr2, uint32_t count);
 uint32_t strlen(uint8_t* ptr);
-uint32_t strtok(char* str, char* pattern);
+char* strtok(char* str, char* pattern);
 
 #endif
 
